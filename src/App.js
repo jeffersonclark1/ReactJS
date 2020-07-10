27a2364f22e3aa49249c9891733aa0119f0526db
Jefferson Clark <jeffersonclarkti@gmail.com>
2020-07-10 11:58:21 -0300

    Detalhe de pagina logico para passar dados via URL

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Routes from './routes';
+
+import './styles.css';
 
 import Header from './components/Header';
 
-import './styles.css';
 import Main from './pages/main'
 
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
